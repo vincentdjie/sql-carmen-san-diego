db2f5f4b699ca5a05eda25262b4919d3b308846f
Sarah King <009kings@gmail.com>
2019-04-05 14:25:35 -0700

    Update clues.sql

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -38,8 +38,7 @@
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
  -- landing dock.
-
--- Clue #8: Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
+-- Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but
 -- if we can crack it, we can finally put her where she belongs – behind bars.
 
 -- Our play date of late has been unusually fun –
